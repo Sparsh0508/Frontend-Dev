a86437cd4c52e17602522ece0e56e5c74de09967
Sparsh0508 <157520563+Sparsh0508@users.noreply.github.com>
2025-11-13 23:00:43 +0530

    Add files via upload

---
+++ JavaScript/String.js
@@ -0,0 +1,26 @@
+let a = "hi";
+let b = "hi";
+let c = hi;
+
+let str1 = new String('hi');
+
+let d = a+" "+b;
+let e = a.concat(b);
+let f = `Hii${b}`;
+console.log(f);
+
+console.log(b.length);
+
+console.log(b.toLowerCase());
+
+console.log(b.toUpperCase());
+
+b.trim();
+
+console.log("Hello".includes("He")); 
+
+console.log(("Hello".slice(1,4)));
+
+console.log("Hello World".substring(0,4));
+
+console.log("Hello".split(""));
